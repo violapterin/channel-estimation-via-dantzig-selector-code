Fix norm normalization of P

src/functions.py
@@ -48,7 +48,7 @@ def execute (ver):
         lst_method.append (lambda x, y: ddss_theory (x, y, ver))
         for c_0 in multiple_values (cst.NUM_GAMMA_DS (ver)):
             lst_legend.append ("DS, $\gamma$ = " + '%.2f' % c_0 + "$\sigma$")
-            lst_method.append (lambda x, y, c = c_0: ddss_llpp (x, c * y, ver))
+            lst_method.append (lambda x, y, c = c_0: ddss_llpp_2 (x, c * y, ver))
 
     assert (len (lst_method) == len (lst_legend))
     num_method = len (lst_method)
@@ -67,10 +67,10 @@ def execute (ver):
         norm_hh = 0
         for _ in range (cst.NUM_REPEAT (ver)):
             count_prog += 1
-            ff_bb = pick_ff_bb (ver)
-            ff_rr = pick_ff_rr (ver)
-            ww_bb = pick_ww_bb (ver)
-            ww_rr = pick_ww_rr (ver)
+            ff_bb = pick_mat_bb (ver).T
+            ff_rr = pick_mat_rr (ver).T
+            ww_bb = pick_mat_bb (ver)
+            ww_rr = pick_mat_rr (ver)
             hh = pick_hh (ver)
             zz = pick_zz (ver)
             kk = get_kk (ver)
@@ -255,11 +255,17 @@ def ddss_direct (est, gamma, ver):
         est.g_rep_hat = np.linalg.pinv (est.pp_rep) @ est.y_rep
     est.convert ()
 
-def ddss_llpp (est, gamma, ver):
+def ddss_llpp_1 (est, gamma, ver):
     est.refresh ()
     i = np.ones ((2 * cst.NN_H (ver)))
     k = est.pp_rep.T @ est.y_rep
     qq = est.pp_rep.T @ est.pp_rep
+
+    #d = np.linalg.norm (qq)
+    #qq /= d
+    #i /= d
+    #k /= d
+    #gamma /= d
 
     g = cp.Variable ((2 * cst.NN_H (ver)))
     f = cp.Variable ((2 * cst.NN_H (ver)))
@@ -274,17 +280,60 @@ def ddss_llpp (est, gamma, ver):
 
     try:
         #prob.solve (solver = cp.GLPK, glpk = {'msg_lev': 'GLP_MSG_OFF'})
-        prob.solve (solver = cp.SCS)
+        prob.solve (solver = cp.ECOS)
         #prob.solve (
         #    solver = cp.ECOS,
         #    max_iters = cst.ITER_MAX_CVX (),
         #    abstol = cst.TOLERANCE_ABS_CVX (),
         #    reltol = cst.TOLERANCE_REL_CVX ())
+
+        #est.g_rep_hat = d * g.value
         est.g_rep_hat = g.value
     except cp.error.SolverError:
         print ("Dantzig Selector fails to solve the program!", flush = True)
         est.g_rep_hat = np.linalg.pinv (est.pp_rep) @ est.y_rep
     est.convert ()
+
+def ddss_llpp_2 (est, gamma, ver):
+    est.refresh ()
+    i = np.ones ((2 * cst.NN_H (ver)))
+    k = est.pp_rep.T @ est.y_rep
+    qq = est.pp_rep.T @ est.pp_rep
+
+    #d = np.linalg.norm (qq)
+    #qq /= d
+    #i /= d
+    #k /= d
+    #gamma /= d
+
+    u = cp.Variable ((2 * cst.NN_H (ver)))
+    v = cp.Variable ((2 * cst.NN_H (ver)))
+    t = cp.Variable (1)
+
+    prob = cp.Problem (
+        cp.Minimize (i.T @ u + i.T @ v + gamma * t),
+        [- t <= 0,
+            - u <= 0,
+            - v <= 0,
+            qq @ u - qq @ v - t * i <= k,
+            - qq @ u + qq @ v - t * i <= -k])
+
+    try:
+        #prob.solve (solver = cp.GLPK, glpk = {'msg_lev': 'GLP_MSG_OFF'})
+        prob.solve (solver = cp.ECOS)
+        #prob.solve (
+        #    solver = cp.ECOS,
+        #    max_iters = cst.ITER_MAX_CVX (),
+        #    abstol = cst.TOLERANCE_ABS_CVX (),
+        #    reltol = cst.TOLERANCE_REL_CVX ())
+
+        #est.g_rep_hat = d * (u.value - v.value)
+        est.g_rep_hat = u.value - v.value
+    except cp.error.SolverError:
+        print ("Dantzig Selector fails to solve the program!", flush = True)
+        est.g_rep_hat = np.linalg.pinv (est.pp_rep) @ est.y_rep
+    est.convert ()
+
 
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
@@ -390,23 +439,15 @@ def mat_complex_normal (nn_1, nn_2):
 def pick_zz (ver):
     return mat_complex_normal (cst.NN_YY (ver), cst.NN_YY (ver))
 
-def pick_ff_bb (ver):
-    return (mat_complex_normal (cst.NN_RR (ver), cst.NN_YY (ver))
-    / np.sqrt (cst.NN_YY (ver)))
+def pick_mat_bb (ver):
+    return (mat_complex_normal (cst.NN_YY (ver), cst.NN_RR (ver)) / np.sqrt (cst.NN_YY (ver)))
+    #return (mat_complex_normal (cst.NN_YY (ver), cst.NN_RR (ver)))
 
-def pick_ww_bb (ver):
-    return (mat_complex_normal (cst.NN_YY (ver), cst.NN_RR (ver))
-    / np.sqrt (cst.NN_YY (ver)))
-
-def pick_ff_rr (ver):
-    kk = get_kk (ver)
-    kk_ss = kk [:, random.sample (list (range (cst.NN_HH(ver))), cst.NN_RR(ver))]
-    return kk_ss / np.sqrt (cst.NN_RR (ver))
-
-def pick_ww_rr (ver):
-    kk = get_kk (ver)
+def pick_mat_rr (ver):
+    kk = np.sqrt (cst.NN_HH (ver)) * get_kk (ver)
     kk_ss = kk [random.sample (list (range (cst.NN_HH(ver))), cst.NN_RR(ver)), :]
     return kk_ss / np.sqrt (cst.NN_RR (ver))
+    #return kk_ss
 
 def pick_hh (ver):
     ret =np.zeros ((cst.NN_HH (ver), cst.NN_HH (ver)), dtype=complex)

src/test.py
@@ -5,60 +5,86 @@ import os
 import numpy as np
 import scipy as sp
 import cvxpy as cp
+import time
 from enum import Enum
 
-#import constants as cst
-#import classes as cls
-#import functions as fct
+import constants as cst
+import classes as cls
+import functions as fct
 import random
 
-aa = np.eye (3)
-zz = np.zeros ((3,3))
-aa = sp.sparse.coo_matrix (aa)
-zz = sp.sparse.coo_matrix (zz)
-print (aa)
-print (zz)
+def foo (a):
+    a += 1
+    print (a)
+
+def mask_low (arr):
+    arr = arr+0.01
+    s =arr.shape
+    num_supp = int (np.sqrt (s[0] * s[1])) + 1
+    arr_abs = abs(arr)
+    arr_vec = fct.vectorize (arr)
+    arr_abs_vec = fct.vectorize (arr_abs)
+    idx_mag = np.argpartition (arr_abs_vec, num_supp)
+    idx_low = idx_mag [0:1-num_supp]
+    arr_vec [idx_low] = 0
+    arr = arr_vec
+    arr = fct.inv_vectorize (arr_vec, s [0], s [1])
+    return arr
+
+
+a = 3
+foo (a)
+foo (a)
+print (a)
 quit ()
 
-a = np.array ([[1,0], [0,5]])
-b = a
-a = sp.sparse.coo_matrix (a)
-c = a.T @ a + a
-print (np.linalg.norm (c.toarray()))
-#print (np.linalg.norm (b.T @ a))
-#print (np.linalg.norm (a.T @ a))
-quit()
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
 
-a =np.array ([[1,1,2,2,3,3,4,4], [5,5,6,6,7,7,8,8]])
-b =a [:,0:2]
-cc =np.block ([[np.eye (3), np.eye (3)], [-np.eye (3), np.eye (3)]])
-e1 =np.array ([1,2,3,4])
-e2 =np.ones ((4))
-d =np.block ([e1, e2])
-print (b)
-quit()
+mm = 20
+hold_sum_det =0
+for m in range (mm):
+    ver = cls.Version (cls.Size.MEDIUM, cls.Focus.ASSORTED)
+    ff_bb =fct.pick_mat_bb (ver).T
+    ff_rr =fct.pick_mat_rr (ver).T
+    ww_bb =fct.pick_mat_bb (ver)
+    ww_rr =fct.pick_mat_rr (ver)
 
-a =np.array ([[1,2], [3,4]])
-b =np.array ([[2,4], [6,8]])
-c =np.array ([[0,0], [0,0]])
-d =np.array ([[0,0], [0,0]])
-aa =np.block([[a,b],[c,d]])
+    kk = fct.get_kk (ver)
+    pp = np.kron (ff_bb.T @ ff_rr.T @ kk.conj(), ww_bb @ ww_rr @ kk)
+    d = np.linalg.det (pp.T @ pp)
+    hold_sum_det += d
+print ("det: ", hold_sum_det / mm)
+quit ()
+
+
+ver = cls.Version (cls.Size.MEDIUM, cls.Focus.ASSORTED)
+ww_bb =fct.pick_mat_bb (ver)
+ww_rr =fct.pick_mat_rr (ver)
+print (np.linalg.det (ww_bb.conj().T @ ww_bb))
+print (np.linalg.det (ww_rr.conj().T @ ww_rr))
+print (np.linalg.det (ww_bb @ ww_bb.conj().T))
+print (np.linalg.det (ww_rr @ ww_rr.conj().T))
+quit ()
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+
+aa = np.zeros ((4,3))
+aa [2][1] =1
+aa [0][1] =3
 print (aa)
-quit()
-
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
-
-
-for _ in range (500):
-    print ("experiment:", _)
-    pp =np.random.normal (0, 1, (10,50))
-    y =np.random.normal (0, 1, (10))
-    g = cp.Variable (50)
-    prob = cp.Problem (
-        cp.Minimize (cp.norm (g, 1)),
-        [cp.norm (pp.conj().T @ (pp @ g - y), "inf") <= 0.5])
-    prob.solve (solver = cp.CVXOPT)
-    g_hat = g.value
-    print (np.linalg.norm (g_hat, ord=1))
+aa = mask_low (aa)
+print (aa)
+quit ()
 
 
+quit ()
+
+
+mm = 10
+t1 = time.time()
+for _ in range (mm):
+    np.linalg.inv (pp.T @ pp)
+    t2 = time.time()
+print ("time: ", (t2-t1)/mm)
+
+quit ()